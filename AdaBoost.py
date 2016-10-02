75919e10b1af1565ec82b5da9acba18e2de6193c
Nishi Kazuya <kwac@Kwacs-MacBook-Pro.local>
2016-10-02 09:03:12 +0900

    大まかにプログラム内容を言葉で記述

--- AdaBoost.py
+++ AdaBoost.py
@@ -1,2 +1,25 @@
 # -*- coding: utf-8 -*-
 
+import random
+import math
+from matplotlib import pyplot as plt
+import numpy as np
+
+#弱識別器はbagingで使ったやつの中で誤率1/2以下のものを選択する
+
+#（１）弱学習器の選別
+
+#（２）必要な重みを用意(w,α)
+
+#（３）重みWの初期化
+
+#（４）学習器の数だけ繰り返す
+
+    #（４−１）誤差関数を求める
+    
+    #（４−２）重みαを計算する
+    
+    #（４−３）重みwの更新
+    
+#（５）識別結果の出力（プロット）
+
